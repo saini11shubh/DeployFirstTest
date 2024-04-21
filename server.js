7e43620b0f6522d40ee3a8febff95d73fad89210
saini11shubh <107746129+saini11shubh@users.noreply.github.com>
2024-04-21 18:39:04 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express();
 app.get("/", (req, res) => {
+    console.log("Hello deployent")
+    res.send("Hello")
+})
+app.post("/test", (req, res) => {
+    console.log("Hello deployent Post PRogram")
     res.send("Hello")
 })
 app.listen(3000, () => {
